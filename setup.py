49bf6ec5d8b5ffa85399e983c97b2460137176db
Gene Lee <genejaelee@gmail.com>
2018-01-17 00:08:20 -0500

    Remove py2exe windows requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #! /usr/bin/python
 from distutils.core import setup
-import py2exe
 
 
-opts = {"py2exe":{
-    "dll_excludes":["MSWSOCK.dll", "IPHLPAPI.dll", "MSWSOCK.dll", "WINNSI.dll", "WTSAPI32.dll"],
-    "packages":["email"]
-    }}
+opts = {}
 
 setup( options = opts, windows = [
                         {
